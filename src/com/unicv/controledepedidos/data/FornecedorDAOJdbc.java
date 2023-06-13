e11fb14ca252d27aeeeae955470d4b379bd2ec0c
erik <erik@Vulcano>
2023-06-13 10:26:57 -0100

    feat: modificação os metodos add das classes DAOs.

--- src/com/unicv/controledepedidos/data/FornecedorDAOJdbc.java
+++ src/com/unicv/controledepedidos/data/FornecedorDAOJdbc.java
@@ -22,12 +22,16 @@ public class FornecedorDAOJdbc implements FornecedorDAO {
     public void add(Fornecedor fornecedor) throws DaoException {
         String sql = "insert into fornecedores(codigo_fornecedor, nome_fornecedor, pais)"
                 + "values(?, ?, ?)";
-        try (Connection conn = JDBCUtil.getConnection(); 
-                PreparedStatement pstmt = conn.prepareStatement(sql);) {
+        try (Connection conn = JDBCUtil.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql);) {
             pstmt.setInt(1, fornecedor.getCodigo());
             pstmt.setString(2, fornecedor.getNome());
             pstmt.setString(3, fornecedor.getPais());
             pstmt.executeUpdate();
+            ResultSet rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                int id = rs.getInt(1);
+                fornecedor.setId(id);
+            }
             conn.commit();
         } catch (SQLException ex) {
             throw new DaoException(ex);
@@ -38,8 +42,7 @@ public class FornecedorDAOJdbc implements FornecedorDAO {
     public void remove(int id) throws DaoException {
         String sql = "delete from fornecedores "
                 + "where id_fornecedor = ?";
-        try (Connection conn = JDBCUtil.getConnection(); 
-                PreparedStatement pstmt = conn.prepareStatement(sql);) {
+        try (Connection conn = JDBCUtil.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql);) {
             pstmt.setInt(1, id);
             pstmt.executeUpdate();
             conn.commit();
@@ -52,12 +55,11 @@ public class FornecedorDAOJdbc implements FornecedorDAO {
     public void update(Fornecedor fornecedor) throws DaoException {
         String sql = "update fornecedores set codigo_fornecedor = ?, nome_fornecedor = ?, pais = ? "
                 + "where id_fornecedor = ?";
-        try (Connection conn = JDBCUtil.getConnection(); 
-                PreparedStatement pstmt = conn.prepareStatement(sql);) {
+        try (Connection conn = JDBCUtil.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql);) {
             pstmt.setInt(1, fornecedor.getCodigo());
             pstmt.setString(2, fornecedor.getNome());
             pstmt.setString(3, fornecedor.getPais());
-            pstmt.setInt(4, fornecedor.getId());            
+            pstmt.setInt(4, fornecedor.getId());
             pstmt.executeUpdate();
             conn.commit();
         } catch (SQLException ex) {
@@ -70,17 +72,16 @@ public class FornecedorDAOJdbc implements FornecedorDAO {
         String sql = "select * from fornecedores "
                 + "where codigo_fornecedor = ?";
         Optional<Fornecedor> optionalFornecedor = Optional.empty();
-        try (Connection conn = JDBCUtil.getConnection(); 
-                PreparedStatement pstmt = conn.prepareStatement(sql);) {
+        try (Connection conn = JDBCUtil.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql);) {
             pstmt.setInt(1, codigo);
-            try (ResultSet rs = pstmt.executeQuery();) {              
+            try (ResultSet rs = pstmt.executeQuery();) {
                 if (rs.next()) {
                     Fornecedor fornecedor = new Fornecedor();
                     fornecedor.setCodigo(rs.getInt("codigo_fornecedor"));
                     fornecedor.setNome(rs.getString("nome_fornecedor"));
                     fornecedor.setPais(rs.getString("pais"));
                     optionalFornecedor = Optional.of(fornecedor);
-                }              
+                }
                 return optionalFornecedor;
             }
         } catch (SQLException ex) {
@@ -92,8 +93,7 @@ public class FornecedorDAOJdbc implements FornecedorDAO {
     public List<Fornecedor> findByNome(String nome) throws DaoException {
         String sql = "select * from fornecedores "
                 + "where nome_fornecedor like ?";
-        try (Connection conn = JDBCUtil.getConnection(); 
-                PreparedStatement pstmt = conn.prepareStatement(sql);) {
+        try (Connection conn = JDBCUtil.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql);) {
             pstmt.setString(1, nome + "%");
             try (ResultSet rs = pstmt.executeQuery();) {
                 List<Fornecedor> listaFornecedores = new ArrayList<>();
@@ -115,8 +115,7 @@ public class FornecedorDAOJdbc implements FornecedorDAO {
     @Override
     public List<Fornecedor> findAll() throws DaoException {
         String sql = "select * from fornecedores";
-        try (Connection conn = JDBCUtil.getConnection(); 
-                PreparedStatement pstmt = conn.prepareStatement(sql);) {
+        try (Connection conn = JDBCUtil.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql);) {
             try (ResultSet rs = pstmt.executeQuery();) {
                 List<Fornecedor> listaFornecedores = new ArrayList<>();
                 while (rs.next()) {
